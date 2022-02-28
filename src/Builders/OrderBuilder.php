e2cd0ac40a6f09f08a7d2deb122aa35b7731f423
Aleksandar <mitevskialeksandar@outlook.com>
2022-02-28 03:32:01 +0100

    method,model

--- src/Builders/OrderBuilder.php
+++ src/Builders/OrderBuilder.php
@@ -8,4 +8,17 @@ class OrderBuilder extends Builder
 {
     protected $entity = 'orders';
     protected $model  = Order::class;
+
+    public function createShipmentForOrder($orderId, $data = [])
+    {
+        return $this->request->handleWithExceptions( function () use ( $orderId, $data ) {
+            $response = $this->request->client->post( "order/" . urlencode($orderId) . "/ship", [
+                'json' => $data,
+            ] );
+
+            $responseData = json_decode( (string) $response->getBody() );
+
+            return new $this->model( $responseData );
+        } );
+    }
 }
---
+++ src/Models/OrderShipment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KgBot\Magento\Models;
+
+use KgBot\Magento\Utils\Model;
+
+class OrderShipment extends Model
+{
+    protected $entity     = 'shipment';
+    protected $primaryKey = 'entity_id';
+}
