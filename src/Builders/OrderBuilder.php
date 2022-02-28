81569888c64df7c464f6bb3105f42b9336cc00c5
Aleksandar <mitevskialeksandar@outlook.com>
2022-02-28 03:33:05 +0100

    test

--- src/Builders/OrderBuilder.php
+++ src/Builders/OrderBuilder.php
@@ -7,5 +7,5 @@ use KgBot\Magento\Models\Order;
 class OrderBuilder extends Builder
 {
     protected $entity = 'orders';
-    protected $model  = Order::class;
+    protected $model  = Order::class; 
 }
